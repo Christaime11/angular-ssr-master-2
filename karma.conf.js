a7e13f3d041238be9e40657716b351d9648204d8
christaime <linusague@gmail.com>
2021-09-13 22:17:13 +0200

    lipton

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-ssr-test'),
+      dir: require('path').join(__dirname, './coverage/thetiptop'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ global['localStorage'] = localStorage;
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/angular-ssr-test/browser');
+  const distFolder = join(process.cwd(), 'dist/thetiptop/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
